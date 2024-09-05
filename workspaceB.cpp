b3bf173c7b73345d9bda84a9f4165412026295ea
Leo Cui <leomyth123@gmail.com>
2024-09-05 07:36:44 -0700

    current save

--- workspaceB.cpp
+++ workspaceB.cpp
@@ -3,15 +3,59 @@
 #include <string>
 #include <pthread.h>
 #include "json/json.h"
-#include "libaktualizr/packagemanagerinterface.h"
+// #include "libaktualizr/packagemanagerinterface.h"
 #include "libaktualizr/packagemanagerfactory.h"
-#include "libaktualizr/config.h"
+// #include "libaktualizr/config.h"
 #include "http/httpclient.h"
-#include "storage/invstorage.h"
+// #include "storage/invstorage.h"
 
 extern "C" {
 #include "network_ipc.h"
 }
+
+#include <sys/statvfs.h>
+#include <boost/filesystem.hpp>
+#include <chrono>
+
+#include "crypto/crypto.h"
+#include "crypto/keymanager.h"
+#include "logging/logging.h"
+#include "uptane/exceptions.h"
+#include "uptane/fetcher.h"
+#include "utilities/apiqueue.h"
+
+struct DownloadMetaStruct {
+ public:
+  DownloadMetaStruct(Uptane::Target target_in, FetcherProgressCb progress_cb_in, const api::FlowControlToken* token_in)
+      : hash_type{target_in.hashes()[0].type()},
+        target{std::move(target_in)},
+        token{token_in},
+        progress_cb{std::move(progress_cb_in)},
+        time_lastreport{std::chrono::steady_clock::now()} {}
+  uintmax_t downloaded_length{0};
+  unsigned int last_progress{0};
+  std::ofstream fhandle;
+  const Hash::Type hash_type;
+  MultiPartHasher& hasher() {
+    switch (hash_type) {
+      case Hash::Type::kSha256:
+        return sha256_hasher;
+      case Hash::Type::kSha512:
+        return sha512_hasher;
+      default:
+        throw std::runtime_error("Unknown hash algorithm");
+    }
+  }
+  Uptane::Target target;
+  const api::FlowControlToken* token;
+  FetcherProgressCb progress_cb;
+  // each LogProgressInterval msec log dowload progress for big files
+  std::chrono::time_point<std::chrono::steady_clock> time_lastreport;
+
+ private:
+  MultiPartSHA256Hasher sha256_hasher;
+  MultiPartSHA512Hasher sha512_hasher;
+};
 
 Json::Value jsonDataOut;
 
@@ -20,11 +64,12 @@ static pthread_cond_t cv_end = PTHREAD_COND_INITIALIZER;
 
 int verbose = 1;
 char buf[256];
-size_t bytes_read;
 
+std::string url = "https://link.storjshare.io/s/juoufh4dg6rfg4jkbcmyu5lvsggq/gsoc/swupdate-torizon-benchmark-image-verdin-imx8mm-20240702064741.swu?download=1";
 std::shared_ptr<HttpInterface> http;
-std::shared_ptr<INvStorage> storage;
-std::shared_ptr<PackageManagerInterface> packageManager;
+// std::shared_ptr<INvStorage> storage;
+// std::shared_ptr<PackageManagerInterface> packageManager;
+DownloadMetaStruct* ds;
 
 int parseJsonFile(const std::string& jsonFilePath, Json::Value& jsonData) {
   std::ifstream jsonFile(jsonFilePath, std::ifstream::binary);
@@ -42,25 +87,42 @@ int parseJsonFile(const std::string& jsonFilePath, Json::Value& jsonData) {
   return 0;
 }
 
-int readimage(char **pbuf, int *size) {
-  std::string target_url = jsonDataOut["target"]["url"].asString();
+static size_t DownloadHandler(char* contents, size_t size, size_t nmemb, void* userp) {
+  assert(userp);
+  ds = static_cast<DownloadMetaStruct*>(userp);
+  size_t downloaded = size * nmemb;
+  uint64_t expected = ds->target.length();
+  if ((ds->downloaded_length + downloaded) > expected) {
+    return downloaded + 1;  // curl will abort if return unexpected size;
+  }
 
-  auto response = http->download(target_url,
-    [](const void* contents, size_t sizeT, size_t nmemb, void* userp) {
-      size_t downloaded = size * nmemb;
-      auto* ds = reinterpret_cast<DownloadState*>(userp);
-      ds->hasher().update(reinterpret_cast<const unsigned char*>(contents), downloaded);
-      return downloaded;
-    },
+  ds->fhandle.write(contents, static_cast<std::streamsize>(downloaded));
+  ds->hasher().update(reinterpret_cast<const unsigned char*>(contents), downloaded);
+  ds->downloaded_length += downloaded;
+  return downloaded;
+}
+
+int readimage(char **pbuf, int *size) {
+  // int ret;
+	// ret = read(fd, buf, sizeof(buf));
+	// *p = buf;
+	// *size = ret;
+	// return ret;
+  // std::string target_url = jsonDataOut["target"]["url"].asString();
+
+  auto response = http->downloadAsync(url,
+    DownloadHandler,
     nullptr,  // ProgressHandler can be added if needed
-    *pbuf,
-    0
+    ds->get(), // unsure
+    static_cast<curl_off_t>(ds->downloaded_length),
+    nullptr //curlhandler easyp*
   );
 
-  // if (response == CURLE_OK) {
-  //   *size = bytes_read;
-  //   return 0;
-  // }
+  if (response.isOk()) {
+    *size = response;
+    return size;
+  }
+
   return -1;
 }
 
@@ -80,13 +142,14 @@ int end(RECOVERY_STATUS status) {
   if (status == SUCCESS) {
     std::printf("Executing post-update actions.\n");
     // Finalize the hash
-    // https://github.com/rborn-tx/aktualizr/blob/ada682bac636f351d7811a498efbf5ff2b64760c/src/libaktualizr/package_manager/ostreemanager.cc#L384
-    // auto final_hash = packageManager->getCurrentHash();
-    // if (final_hash != jsonDataOut["custom"]["swupdate"]["rawHashes"]["sha256"].asString()) {
-    //   std::fprintf(stderr, "Running post-update failed!\n");
-    //   end_status = EXIT_FAILURE;
-    //   // does end_status actually cancel the update?
-    // }
+    auto final_hash = ds->hasher().getHash().HashString();
+    std::string val = "";
+    // val = jsonDataOut["custom"]["swupdate"]["rawHashes"]["sha256"].asString();
+    if (final_hash != val) {
+      std::fprintf(stderr, "Running post-update failed!\n");
+      end_status = EXIT_FAILURE;
+      // does end_status actually cancel the update?
+    }
   }
 
   pthread_mutex_lock(&mymutex);
@@ -96,14 +159,15 @@ int end(RECOVERY_STATUS status) {
   return end_status;
 }
 
-int swupdate_test_func(const std::string& url) {
+int swupdate_test_func() {
   struct swupdate_request req;
   int rc;
 
   // Initialize the actual `PackageManagerInterface`
-  PackageConfig pconfig;
-  BootloaderConfig bconfig;
-  packageManager = PackageManagerFactory::makePackageManager(pconfig, bconfig, storage, http);
+  http = std::make_shared<HttpClient>();
+  // PackageConfig pconfig;
+  // BootloaderConfig bconfig;
+  // packageManager = PackageManagerFactory::makePackageManager(pconfig, bconfig, storage, http);
 
   swupdate_prepare_req(&req);
 
@@ -122,12 +186,11 @@ int swupdate_test_func(const std::string& url) {
 }
 
 int main() {
-  std::string url = "https://link.storjshare.io/s/juoufh4dg6rfg4jkbcmyu5lvsggq/gsoc/swupdate-torizon-benchmark-image-verdin-imx8mm-20240702064741.swu?download=1";
-  std::string jsonFilePath = "../test.json";
+  // std::string jsonFilePath = "../test.json";
 
-  if (parseJsonFile(jsonFilePath, jsonDataOut) == 0) {
-      swupdate_test_func(url);
-  }
+  // if (parseJsonFile(jsonFilePath, jsonDataOut) == 0) {
+  swupdate_test_func();
+  // }
 
   return 0;
 }
